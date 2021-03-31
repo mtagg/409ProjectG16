30358d0f7f941943df86ae23f893c9700e586613
Dylan <75186098+mcdylan38@users.noreply.github.com>
2021-03-31 11:32:06 -0600

    Added test for all functions
    Can't get last test to expect an SQLException

--- src/edu/ucalgary/ensf409/DatabaseDriverTest.java
+++ src/edu/ucalgary/ensf409/DatabaseDriverTest.java
@@ -86,6 +86,3 @@ public class DatabaseDriverTest {
     }
     */
 }
-
-// javac -cp .;junit-4.13.2.jar;hamcrest-core-1.3.jar;mysql-connector-java-8.0.23.jar; edu/ucalgary/ensf409/DatabaseDriverTest.java
-// java -cp .;junit-4.13.2.jar;hamcrest-core-1.3.jar;mysql-connector-java-8.0.23.jar; org.junit.runner.JUnitCore edu.ucalgary.ensf409.DatabaseDriverTest
