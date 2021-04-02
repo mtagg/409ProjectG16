45aff9099f09717c39ef7dafda4009712473c155
unknown <dylancmah3@gmail.com>
2021-04-02 17:09:17 -0600

    Added tests for getters

--- src/edu/ucalgary/ensf409/InventorySystemTest.java
+++ src/edu/ucalgary/ensf409/InventorySystemTest.java
@@ -1,28 +1,49 @@
+/**
+ * ENSF 409 - Final Project
+ * Author: Dylan Mah (30086580)
+ */
+
 package edu.ucalgary.ensf409;
+import static org.junit.Assert.*;
 import org.junit.*;
+import org.junit.rules.ExpectedException;
 
 public class InventorySystemTest {
-    public InventorySystemTest() {
+    static InventorySystem test=new InventorySystem();
+
+    @BeforeClass
+    public static void setup(){
+        test.run();
+    }
+
+    @Rule public ExpectedException error=ExpectedException.none();
+
+    /*
+    @Test
+    // Test to see if run executed with no errors
+    public void testRun(){
+        test.run();
+    }
+    */
+
+    @Test
+    // Test if variable category is updated when user inputs values
+    public void testGetCategory(){
+        assertFalse(test.getCategory().isEmpty());
     }
 
     @Test
-    public void testRun() { //TestRun to testRun
-        //test if error message happens
-
+   // Test if variable type is updated when user inputs values
+    public void testGetType(){
+        assertFalse(test.getCategory().isEmpty());
     }
 
     @Test
-    public void testGetCategory() {
-
-    }
-
-    @Test
-    public void testGetType() {
-
-    }
-
-    @Test
-    public void testGetQuantity() {
-
+    // Test if variable quantity is updated when user inputs values
+    public void testGetQuantity(){
+        assertNotEquals(test.getQuantity(), 0);
     }
 }
+
+// javac -cp .;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar;lib/system-rules-1.19.0.jar;lib/mysql-connector-java-8.0.23.jar;. edu/ucalgary/ensf409/InventorySystemTest.java
+// java -cp .;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar;lib/system-rules-1.19.0.jar;lib/mysql-connector-java-8.0.23.jar;. org.junit.runner.JUnitCore edu.ucalgary.ensf409.InventorySystemTest
