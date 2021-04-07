cf01f5d43846b0eaec5027eb0f8bc2cdd6ed7eb7
unknown <dylancmah3@gmail.com>
2021-04-07 14:10:33 -0600

    Updated formatting and some comments

--- src/edu/ucalgary/ensf409/InventorySystem.java
+++ src/edu/ucalgary/ensf409/InventorySystem.java
@@ -29,7 +29,7 @@ public class InventorySystem {
     private int quantity;
 
     /**
-     * Constructor for Inventory System creates Scanner object to pass through
+     * Constructor for Inventory System. Creates Scanner object to pass through
      * InventoryManager to DatabaseDriver for MySQL credentials request
      */
     public InventorySystem() {
@@ -38,11 +38,12 @@ public class InventorySystem {
     }
 
     /**
-     * Main run method Starts UI and calls necessary methods will request user input
-     * until correct inputs are received closes Scanner object and InventoryManager
+     * Run starts UI and calls necessary methods that will request user input
+     * until correct inputs are received. Closes Scanner object and InventoryManager
      * to close MySQL connection
      */
     public void run() {
+        // Loops to check categories, types and quantities
         do {
             this.category = setCategory();
         } while (this.category == null);
@@ -56,8 +57,11 @@ public class InventorySystem {
         } while (this.quantity == -1);
 
         if(!inventoryManager.pieceFurniture(category, type, quantity)) {
+            // If furniture cannot be created, print error message
             printErrorMessage(category);
         }
+
+        // Release MySQL resources
         this.scan.close();
         this.inventoryManager.close();
     }
@@ -73,14 +77,15 @@ public class InventorySystem {
         message += suggestedManufacturers.get(0);
 		
         if(suggestedManufacturers.size() == 1) {
+            // If there is at only one manufacturer, print the name(s)
             System.out.println(message);
             return;
         }
-		
+
+        // Loop to get other manufacturers (excluding the first one retirved previously)
         for(int i = 1; i < suggestedManufacturers.size(); i++) {
             message += ", " + suggestedManufacturers.get(i);
         }
-		
         System.out.println(message + ".");
     }
 
@@ -94,14 +99,17 @@ public class InventorySystem {
             System.out.println("Enter furniture category:");
             String category = scan.nextLine();
             if (inventoryManager.checkCategory(category)) {
+                // If category exists, return it
                 return category;
             }
             else {
+                // If category doesn't exit, return null
                 System.out.println("Category not found, please retry.");
                 return null;
             }
         } 
 		catch (Exception e) {
+            // If an error occurs when reading inputs, print error message and stack trace
             System.out.println("Invalid Furniture Category Request");
             e.printStackTrace();
             return null;
@@ -119,14 +127,17 @@ public class InventorySystem {
             System.out.println("Enter furniture type:");
             String type = scan.nextLine();
             if (inventoryManager.checkType(this.category, type)) {
+                // If type exists, return it
                 return type;
             }
             else {
+                // If type doesn't exist, return null
                 System.out.println("Type not found, please retry.");
                 return null;
             }
         } 
 		catch (Exception e) {
+            // If an error occurs when reading inputs, print error message and stack trace
             System.out.println("Invalid" + getCategory() + "Type Request");
             e.printStackTrace();
             return null;
@@ -142,15 +153,19 @@ public class InventorySystem {
         try {
             System.out.println("Enter the quantity:");
             int q = scan.nextInt();
+
             if (q >= 0) {
+                // If valid quantity is entered, return it
                 return q;
             }
             else {
+                // If invalid quantity is entered, return -1
                 System.out.println("Integer must be >= 0, please try again.");
                 return -1;
             }
         } 
 		catch (Exception e) {
+            // If an error occurs when reading inputs, output error message and stack trace
             System.out.println("Invalid Furniture Quantity Request");
             e.printStackTrace();
             return 0;
@@ -185,11 +200,11 @@ public class InventorySystem {
     }
 
     /**
-     * main method, creates new instance of inventorySystem and calls run method to
+     * Main method. Creates a new instance of inventorySystem and calls the run method to
      * initialize the order request and subsequent methods to determine outcome of
-     * the request
+     * the request.
      * 
-     * @param args unused for our program
+     * @param args command line arguments that are unused for our program
      */
     public static void main(String [] args) {
         InventorySystem inventorySystem = new InventorySystem();
