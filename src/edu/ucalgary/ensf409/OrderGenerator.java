1d1829fb2592277d3b04d61078d0b75308955557
unknown <dylancmah3@gmail.com>
2021-04-06 19:50:07 -0600

    Added documentation and edited formatting

--- src/edu/ucalgary/ensf409/OrderGenerator.java
+++ src/edu/ucalgary/ensf409/OrderGenerator.java
@@ -22,10 +22,11 @@ import java.util.ArrayList;
  * OrderGenerator util class
  * Allows an order to be generated.
  */
+ 
 public class OrderGenerator {
-
     /**
      * Method to generate an order form following an order request by the user
+	 *
      * @param furniture array of furniture to order
      * @param price total price of order
      * @param category category of furniture requested
@@ -33,10 +34,11 @@ public class OrderGenerator {
      * @param quantity quantity of furniture requested
      * @return true if writer opened/closed successfuly, else, false
      */
-    public boolean generateOrder(ArrayList<Furniture> furniture, int price, String category, String type,
-            int quantity) {
+    public boolean generateOrder(ArrayList<Furniture> furniture, int price, String category, String type, int quantity) {
         FileWriter writer = null;
+		
         try {
+			// Writing to output file
             writer = new FileWriter("orderform.txt");
             writer.write("Furniture Order Form\n\n");
             writer.write("Faculty Name:\n");
@@ -44,27 +46,40 @@ public class OrderGenerator {
             writer.write("Date:\n\n");
             writer.write("Original Request: " + type + " " + category + ", " + quantity);
             writer.write("\n\nItems Ordered\n");
-            for(Furniture f : furniture)
+			
+            for(Furniture f : furniture){
+				// Loop to get the IDs of the components of the furniture in the array list
                 writer.write("ID: " + f.getID() + "\n");
+			}
+			
             writer.write("\nTotal Price: $" + price);
 
-        } catch (Exception e) {
+        } 
+		catch (Exception e) {
+			// If an error occured when writing to the file, print the stack trace
             e.printStackTrace();
-        } finally {
+        } 
+		finally {
             if (writer != null) {
+				// If writing to the output file was successful, try to close the output stream, print a success
+				// message and return true
                 try {
                 writer.flush();
                 writer.close();
                 File f = new File("orderform.txt");
                 System.out.println("Order created at path: " + f.getAbsolutePath());
                 return true;
-            } catch (Exception e) {
+            } 
+			catch (Exception e) {
+				// If the filewriter could not be closed, print an error message, the stack trace and return false
                 System.out.println("Could not close filewriter");
-                    e.printStackTrace();
-                    return false;
-
+				e.printStackTrace();
+				return false;
                 }
             }
-        }return false; //if writer never opened
+        }
+		
+		//if writer never opened...
+		return false; 
     }
 }
